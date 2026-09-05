0ec7aa5699300186a9e118fee62873331c2b5f59
Juan Sebastian Castaño Aros <juan_s.castano@uao.edu.co>
2026-09-05 00:00:25 -0500

    Creacion de carpetas en

---
+++ ros2_ws/src/carro_trepador_teleop/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'carro_trepador_teleop'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='juan',
+    maintainer_email='juan_s.castano@uao.edu.co',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
